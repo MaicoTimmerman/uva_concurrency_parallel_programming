9ff0230b30c542e30cd85ec35e410fa1fca7b26a
Robin Klusman <robinklusman@gmail.com>
2014-11-12 17:06:57 +0100

    Final simulate.c started reduction

---
+++ 2wk/assign2_2/reduction.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Code copied from PDF */
+double sum (double* vec, int len)
+{
+    int i;
+    double accu = 0.0;
+    for (i = 0; i < len; i++) {
+        accu = accu + vec[i];
+    }
+    return accu;
+}
+
+
+double reduce (double (fun)(double, double),
+        double* vec, int len, double neutral)
+{
+    int i ;
+    double accu = neutral;
+    for (i = 0; i < len; i++) {
+        accu = fun(accu, vec[i]);
+    }
+    return accu;
+}
