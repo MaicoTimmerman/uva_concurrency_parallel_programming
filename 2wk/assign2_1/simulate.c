f41e1e10eb8f2d5750c4ebc416df75ff9f1cf3aa
Robin Klusman <robinklusman@gmail.com>
2014-11-12 16:37:58 +0100

    OpenMP shit in simulate.c and added assignment pdf to repo

--- 2wk/assign2_1/simulate.c
+++ 2wk/assign2_1/simulate.c
@@ -22,12 +22,35 @@
  * current_array: array of size i_max filled with data for t
  * next_array: array of size i_max. You should fill this with t+1
  */
+
+
+double *old, *cur, *next, *temp;
+int g_t_max, g_i_max;
+
+void calc_wave(void)
+{
+    for (int t = 0; t < g_t_max; t++) {
+        #pragma omp parallel for schedule( static)
+        for (int i = 0; i < g_i_max; i++) {
+            next[i] = (2*cur[i]) - old[i] + (0.15*(cur[i-1] - (2*cur[i] - cur[i+1])));
+        }
+        temp = old;
+        old = cur;
+        cur = next;
+        next = temp;
+    }
+    return;
+}
+
 double *simulate(const int i_max, const int t_max, const int num_threads,
         double *old_array, double *current_array, double *next_array)
 {
-    /*
-     * Your implementation should go here.
-     */
+
+    old = old_array;
+    cur = current_array;
+    next = next_array;
+    g_i_max = i_max;
+    g_t_max = t_max;
 
     return current_array;
 }
