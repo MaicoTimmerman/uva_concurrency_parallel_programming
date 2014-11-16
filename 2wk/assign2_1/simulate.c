b7a8b9146290d4b6fb08881b03360c219e912402
Maico Timmerman <maico.timmerman@gmail.com>
2014-11-16 10:54:32 +0100

    changed some comments

--- 2wk/assign2_1/simulate.c
+++ 2wk/assign2_1/simulate.c
@@ -10,24 +10,15 @@
 #include "simulate.h"
 
 
-/*
- * Executes the entire simulation.
- *
- * Implement your code here.
- *
- * i_max: how many data points are on a single wave
- * t_max: how many iterations the simulation should run
- * num_threads: how many threads to use
- * old_array: array of size i_max filled with data for t-1
- * current_array: array of size i_max filled with data for t
- * next_array: array of size i_max. You should fill this with t+1
- */
 
 
 double *old, *cur, *next, *temp;
 int g_t_max, g_i_max;
 int g_num_threads;
 
+/*
+ * Calculate the wave
+ */
 void calc_wave(void)
 {
     for (int t = 0; t < g_t_max; t++) {
@@ -44,6 +35,16 @@ void calc_wave(void)
     return;
 }
 
+/*
+ * Executes the entire simulation.
+ *
+ * i_max: how many data points are on a single wave
+ * t_max: how many iterations the simulation should run
+ * num_threads: how many threads to use
+ * old_array: array of size i_max filled with data for t-1
+ * current_array: array of size i_max filled with data for t
+ * next_array: array of size i_max. You should fill this with t+1
+ */
 double *simulate(const int i_max, const int t_max, const int num_threads,
         double *old_array, double *current_array, double *next_array)
 {
