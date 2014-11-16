7aa6e1dc10b77cd5eb8603e113d396c046a1cf89
Maico Timmerman <maico.timmerman@gmail.com>
2014-11-16 19:45:09 +0100

    Finished reduction

--- 2wk/assign2_2/reduction.c
+++ 2wk/assign2_2/reduction.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+#include <omp.h>
 
 #include "timer.h"
-
-int g_num_threads;
 
 /* Code copied from PDF */
 double sum(double* vec, int len)
 {
     int i;
     double accu = 0.0;
-    #pragma omp parallel for num_threads(g_num_threads) reduction(+:accu)
+    #pragma omp parallel for reduction(+:accu)
     for (i = 0; i < len; i++) {
         accu = accu + vec[i];
     }
@@ -24,8 +24,9 @@ double reduce(double (fun)(double, double),
 {
     int i;
     double accu = neutral;
-    /* #pragma omp parallel for num_threads(g_num_threads) */
+    #pragma omp parallel for
     for (i = 0; i < len; i++) {
+        #pragma omp critical
         accu = fun(accu, vec[i]);
     }
     return accu;
@@ -34,44 +35,62 @@ double reduce(double (fun)(double, double),
 double reduce2(double (fun)(double, double),
         double* vec, int len, double neutral)
 {
-    int vec_len;
-    int calc_vec_len;
-    double accu;
+    /* We need a copy of the original head of the vector, as well as a vector to work in */
+    double* start_vec = vec;
+    double* calc_vec = NULL;
 
-    accu = neutral;
-    vec_len = len;
-    double *calc_vec = (double*)malloc(sizeof(double)*vec_len);
-    double *free_calc_vec = calc_vec;
+    /* We calculate the largest power of two (n) that will fit in len */
+    int calc_vec_len_copy;
+    int vec_len = len;
+    int calc_vec_len = (int)pow(2, floor(log2(vec_len)));
 
-    /* While the vector has more then one value, keep calculating */
-    while (vec_len > 1) {
-
-        calc_vec_len = pow(2, floor(log2(vec_len)));
-        calc_vec = vec;
-
-        /* Current vector with the length == 2^n */
-        while (calc_vec_len > 1) {
+    double accu = neutral;
 
 
-            /* Halve the vector by passing fun on to all sets */
-            for (int i = 0; i < calc_vec_len; i+=2) {
-                calc_vec[i] = fun(calc_vec[(i*2)], calc_vec[(i*2)+1]);
-            }
+    /* Continue until the whole original vector has been reduced */
+    while (vec_len != 0) {
+
+        /* Remove the current working vector from the original vector. */
+        vec_len -= calc_vec_len;
+
+        /* Save the length of the the current working vector to continue
+         * with the leftovers of the original vector. */
+        calc_vec_len_copy = calc_vec_len;
+
+        /* While the current working vector has not yet been reduced to zero,
+         * keep merging sets of 2 with the function given. */
+        while (calc_vec_len != 1) {
+
+            /* New vector for the results, automatically remove the old. */
+            calc_vec = malloc((calc_vec_len / 2) * sizeof(double));
+
+            /* The merging of sets using the function can be done parallel
+             * Because there are no race conditions in this loop. */
+            #pragma omp parallel for
+            for (int i = 0; i < calc_vec_len; i+=2)
+                calc_vec[i/2] = fun(vec[i], vec[i+1]);
+
+            /* Prepare for next step */
+            vec = calc_vec;
             calc_vec_len = calc_vec_len / 2;
         }
 
-        accu = fun(accu, calc_vec[0]);
+        /* Working vector has been reduced to a single term. Reduce with
+         * the previously done parts. */
+        accu = fun(accu, vec[0]);
 
-        /* The calc_vec is converted into a single number */
-        vec_len = vec_len - calc_vec_len;
-        vec = vec+calc_vec_len;
+
+        /* Shift the original vector beyond all the done indices */
+        start_vec += calc_vec_len_copy;
+        vec = start_vec;
+
+        /* Calculate the length of the new working vector */
+        calc_vec_len = (int)pow(2, floor(log2(vec_len)));
+
     }
 
-    accu = fun(accu, vec[0]);
-
-    free(free_calc_vec);
-
     return accu;
+
 }
 
 double fun(double a, double b) {
@@ -82,57 +101,70 @@ int main(int argc, char *argv[])
 {
     int num_threads;
     int vec_size;
+    int custom_reduce_method;
     double accu;
     double time;
     double *vec;
 
-    if (argc < 3) {
-        printf("Usage: %s num_threads vec_size\n", argv[0]);
+    if (argc < 4) {
+        printf("Usage: %s num_threads vec_size [reduce method]\n", argv[0]);
         printf(" - num_threads: number of threads to use for simulation, "
                 "should be >=1\n");
-        printf(" - vec_size: size of the vector to do reduction on"
+        printf(" - vec_size: size of the vector to do reduction on 10^n"
                 "should be >=10\n");
+        printf(" - [reduce_method]: custom | sequential.");
 
         return EXIT_FAILURE;
     }
 
     num_threads = atoi(argv[1]);
-    vec_size = atoi(argv[2]);
+    vec_size = pow(10, atoi(argv[2]));
     if (num_threads < 1) {
         printf("argument error: num_threads should be >=1.\n");
         return EXIT_FAILURE;
     }
 
-    if (vec_size < 10) {
-        printf("argument error: vec_size should be >=10.\n");
+    if (vec_size < 4) {
+        printf("argument error: vec_size should be >=4.\n");
         return EXIT_FAILURE;
+    }
+
+    if (strcmp(argv[3], "sequential") == 0) {
+        custom_reduce_method = 0;
+    } else {
+        printf("Using custom reduce method");
+        custom_reduce_method = 1;
     }
 
     vec = (double*)malloc(vec_size * sizeof(double));
 
     /* Fill a vector with a sinus values */
-    #pragma omp parallel for num_threads(g_num_threads)
+    #pragma omp parallel for
     for (int i = 0; i < vec_size; i++) {
         vec[i] = sin(i);
     }
 
-    g_num_threads = num_threads;
+
+    omp_set_num_threads(num_threads);
 
     /* Start timing the effeciency of openMP */
     timer_start();
 
     /* Calculate a reduced vector with openMP */
-    /* accu = sum(vec, vec_size); */
-    accu = reduce(fun, vec, vec_size, 0);
-    printf("sum accu: %g\n", accu);
-    accu = reduce2(fun, vec, vec_size, 0);
-    printf("sum accu: %g\n", accu);
-
+    if (custom_reduce_method) {
+        accu = reduce2(fun, vec, vec_size, 0);
+        /* accu = sum(vec, vec_size); */
+    } else {
+        accu = reduce(fun, vec, vec_size, 0);
+    }
     /* Stop timing */
+
     time = timer_end();
+    printf("sum accu: %g\n", accu);
     printf("Took %g seconds\n", time);
 
     free(vec);
+    vec = NULL;
 
     return EXIT_SUCCESS;
 }
