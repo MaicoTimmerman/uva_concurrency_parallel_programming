46be5d15c5b52779f69985d82d4e750c2cb0b469
Maico Timmerman <maico.timmerman@gmail.com>
2014-11-21 16:34:42 +0100

    Working on it

--- 3wk/assign3_1/simulate.c
+++ 3wk/assign3_1/simulate.c
@@ -27,14 +27,33 @@
  * current_array: array of size i_max filled with data for t
  * next_array: array of size i_max. You should fill this with t+1
  */
-double *simulate(const int i_max, const int t_max, double *old_array,
-        double *current_array, double *next_array)
+double *simulate(const int i_max, const int t_max, double *old, double *cur,
+        double *next, const int num_taks, const int task_id)
 {
 
     /*
      * Your implementation should go here.
      */
+    /*  send(left_neighbour, cur[1]) ; */
+    MPI_Send(old[0], 1, MPI_DOUBLE, 0, MSG_WRITE, MPI_COMM_WORLD);
+    MPI_Send(cur[0], 1, MPI_DOUBLE, 0, MSG_WRITE, MPI_COMM_WORLD);
+    /* send(right_neighbour, cur[local_size]) ; */
+    for (int i = 0; i < t_max; i++) {
+        for (int i = 0; i < i_max; i++) {
+            next[i] = (2*cur[i]) - old[i] + (0.15*(cur[i-1] - (2*cur[i] - cur[i+1])));
+        }
+        /* cur[local_size + 1] = receive(right_neighbour); */
+        /* new[local_size ] = ...; */
+        /* send(right_neighbour , new [local_size]); */
+        /* cur[0] = receive(left_neighbour); */
+        /* new[1] = ...; */
+        /* send(left_neighbour , new [1]); */
+        /* old = cur; */
+        /* cur = new; */
+    }
+    /* discard = receive ( left_neighbour ) ; */
+    /* discard = receive ( right_neighbour ) ; */
 
     /* You should return a pointer to the array with the final results. */
-    return current_array;
+    return cur;
 }
--- 3wk/assign3_1/simulate.h
+++ 3wk/assign3_1/simulate.h
@@ -5,4 +5,5 @@
 #pragma once
 
 double *simulate(const int i_max, const int t_max, double *old_array,
-        double *current_array, double *next_array);
+        double *current_array, double *next_array,
+        const int num_taks, const int task_id);
