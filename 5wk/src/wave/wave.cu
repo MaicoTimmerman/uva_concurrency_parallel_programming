778a2f9d577d0195b06c07a92e84ccd2456d3408
Maico Timmerman <maico.timmerman@gmail.com>
2014-12-06 14:14:56 +0100

    Finished wave code

--- 5wk/src/wave/wave.cu
+++ 5wk/src/wave/wave.cu
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#include "timer.h"
-#include <iostream>
 #include <iostream>
 #include <fstream>
+
+#include "timer.h"
 
 using namespace std;
 typedef double (*func_t)(double x);
@@ -34,7 +34,7 @@ double gauss(double x)
 }
 
 /* Fill an double array with the function given */
-void fill(double *array, int offset, int range, double sample_start,
+void fill(float *array, int offset, int range, double sample_start,
         double sample_end, func_t f)
 {
     int i;
@@ -46,70 +46,59 @@ void fill(double *array, int offset, int range, double sample_start,
     }
 }
 
-__global__ void vectorAddKernel(unsigned int i_max, double* old_d, double* cur_d, double* next_d) {
+__global__ void waveKernel(unsigned int i_max, float* old_d, float* cur_d, float* next_d) {
     unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if ((i > 0) || (i < i_max)) {
-        next_d[i] = (2*cur_d[i]) - old_d[i] + (0.15*(cur_d[i-1] - (2*cur_d[i] - cur_d[i+1])));
+    if ((i > 0) && (i < i_max - 1)) {
+        next_d[i] = 2.0f * cur_d[i] - old_d[i] +
+            0.15 * (cur_d[i-1] - (2 * cur_d[i] - cur_d[i+1]));
     } else {
         next_d[i] = 0;
     }
 }
 
 
-void vectorAddCuda(int i_max, int t_max, double* old_h, double* cur_h, double* next_h) {
+float* calcWaveCuda(int i_max, int t_max, float* old_h, float* cur_h, float* next_h) {
     int threadBlockSize = 1024;
-    double *tmp;
+    float *old_d, *cur_d, *next_d, *tmp;
 
     // allocate the vectors on the GPU
-    double* old_d = NULL;
-    checkCudaCall(cudaMalloc((void **) &old_d, i_max * sizeof(double)));
-    if (old_d == NULL) {
-        cout << "could not allocate memory!" << endl;
-        return;
-    }
+    checkCudaCall(cudaMalloc((void **) &old_d, i_max * sizeof(float)));
+    checkCudaCall(cudaMalloc((void **) &cur_d, i_max * sizeof(float)));
+    checkCudaCall(cudaMalloc((void **) &next_d, i_max * sizeof(float)));
 
-    double* cur_d = NULL;
-    checkCudaCall(cudaMalloc((void **) &cur_d, i_max * sizeof(double)));
-    if (cur_d == NULL) {
-        checkCudaCall(cudaFree(old_d));
-        cout << "could not allocate memory!" << endl;
-        return;
-    }
-
-    double* next_d = NULL;
-    checkCudaCall(cudaMalloc((void **) &next_d, i_max * sizeof(double)));
-    if (next_d == NULL) {
-        checkCudaCall(cudaFree(old_d));
-        checkCudaCall(cudaFree(cur_d));
-        cout << "could not allocate memory!" << endl;
-        return;
-    }
+    // copy the original vectors to the GPU
+    checkCudaCall(cudaMemcpy(old_d, old_h, i_max*sizeof(float), cudaMemcpyHostToDevice));
+    checkCudaCall(cudaMemcpy(cur_d, cur_h, i_max*sizeof(float), cudaMemcpyHostToDevice));
 
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
 
-    // copy the original vectors to the GPU
-    checkCudaCall(cudaMemcpy(old_d, old_h, i_max*sizeof(double), cudaMemcpyHostToDevice));
-    checkCudaCall(cudaMemcpy(cur_d, cur_h, i_max*sizeof(double), cudaMemcpyHostToDevice));
 
     // execute kernel
     cudaEventRecord(start, 0);
     for (int i = 0; i < t_max; i++) {
-        vectorAddKernel<<<i_max/threadBlockSize, threadBlockSize>>>(i_max, old_d, cur_d, next_d);
+
+        waveKernel<<<i_max/threadBlockSize, threadBlockSize>>>(i_max, old_d, cur_d, next_d);
+
+        // check whether the kernel invocation was successful
+        checkCudaCall(cudaGetLastError());
+
         tmp = old_d;
         old_d = cur_d;
         cur_d = next_d;
         next_d = tmp;
+
+        tmp = old_h;
+        old_h = cur_h;
+        cur_h = next_h;
+        next_h = tmp;
     }
     cudaEventRecord(stop, 0);
 
-    // check whether the kernel invocation was successful
-    checkCudaCall(cudaGetLastError());
-
     // copy result back
-    checkCudaCall(cudaMemcpy(cur_h, cur_d, i_max * sizeof(double), cudaMemcpyDeviceToHost));
+    checkCudaCall(cudaMemcpy(cur_h, cur_d, i_max * sizeof(float), cudaMemcpyDeviceToHost));
 
     checkCudaCall(cudaFree(old_d));
     checkCudaCall(cudaFree(cur_d));
@@ -118,13 +107,15 @@ void vectorAddCuda(int i_max, int t_max, double* old_h, double* cur_h, double* n
     // print the time the kernel invocation took, without the copies!
     float elapsedTime;
     cudaEventElapsedTime(&elapsedTime, start, stop);
-
     cout << "kernel invocation took " << elapsedTime << " milliseconds" << endl;
+
+    return cur_h;
 }
 
 int main(int argc, char *argv[])
 {
     int t_max, i_max;
+    float *old, *cur, *next, *ret;
 
     /* Parse commandline args: i_max t_max num_threads */
     if (argc < 3) {
@@ -153,13 +144,9 @@ int main(int argc, char *argv[])
     }
 
     /* Allocate and initialize buffers. */
-    double *old = new double[i_max];
-    double *cur = new double[i_max];
-    double *next = new double[i_max];
-
-    memset(old, 0, i_max * sizeof(double));
-    memset(cur, 0, i_max * sizeof(double));
-    memset(next, 0, i_max * sizeof(double));
+   old = new float[i_max]();
+   cur = new float[i_max]();
+   next = new float[i_max]();
 
     /* Fill the first two generations */
     fill(old, 1, i_max/4, 0, 2*3.14, sin);
@@ -170,7 +157,7 @@ int main(int argc, char *argv[])
     vectorAddTimer.start();
 
     /* Call the actual simulation and measure the time */
-    /* ret = simulate(i_max, t_max, num_threads, old, current, next); */
+    ret = calcWaveCuda(i_max, t_max, old, cur, next);
 
     /* Print the time it took */
     vectorAddTimer.stop();
@@ -180,7 +167,7 @@ int main(int argc, char *argv[])
     ofstream returnfile;
     returnfile.open("results.txt", ios::in);
     for (int i = 0; i < i_max; i++) {
-        returnfile << cur[i] << endl;
+        returnfile << ret[i] << endl;
     }
     returnfile.close();
 
