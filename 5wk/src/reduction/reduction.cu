10ee2863fb928d0de93c43eb4bc8486e4dd69b5b
Maico Timmerman <maico.timmerman@gmail.com>
2014-12-07 17:04:02 +0100

    Completed?

--- 5wk/src/reduction/reduction.cu
+++ 5wk/src/reduction/reduction.cu
@@ -36,50 +36,93 @@ float max_array(float a[], int num_elements)
     return max;
 }
 
-__global__ void vectorAddKernel(float* deviceA, float* deviceB, float* deviceResult) {
-    unsigned index = blockIdx.x * blockDim.x + threadIdx.x;
-    deviceResult[index] = deviceA[index] + deviceB[index];
+__global__ void reduce_max_kernel(float* input_d, float* partial_result_d,
+        unsigned int i_max)
+{
+    unsigned global_tid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    /* Let every thread compare a number of the first half of the block to
+       a number in the second half of the block. */
+    for (int offset = blockDim.x / 2; offset > 0; offset >>= 1) {
+
+        /* If the offset is smaller as the threadId, then the thread is not
+           needed during computation */
+        if (global_tid < offset) {
+
+            /* Get the maximum value of both cells */
+            input_d[global_tid] =
+                ((input_d[global_tid] < input_d[global_tid + offset]) ?
+                 input_d[global_tid + offset] : input_d[global_tid]);
+        }
+
+        /* Wait for all threads to update their data, so the next iteration
+           can be started */
+        __syncthreads();
+    }
+
+    if (threadIdx.x == 0) {
+        partial_result_d[blockIdx.x] = input_d[global_tid];
+    }
 }
 
 
-float vectorAddCuda(int i_max, float* list_h) {
-    /* int threadBlockSize = 512; */
-    float *list_d;
+void reduce_max_cuda(int i_max, float *list_h, float result)
+{
+    float *list_d = NULL;
+    float *partial_result_d = NULL;
+    const int block_size = 512;
 
+    /* number of blocks is equal to:
+     * Integer division + extra block for remainder */
+    const int max_blocks = (i_max/block_size) + ((i_max % block_size) ? 1 : 0);
+
+    /* Copy the original vectors to the GPU */
     checkCudaCall(cudaMalloc((void **) &list_d, i_max * sizeof(float)));
+    checkCudaCall(cudaMemcpy(list_d, list_h, i_max * sizeof(float), cudaMemcpyHostToDevice));
+    checkCudaCall(cudaMalloc((void**)&partial_result_d, (max_blocks + 1) * sizeof(float)));
 
+    /* Setup timing  */
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
 
-    // copy the original vectors to the GPU
-    checkCudaCall(cudaMemcpy(list_d, list_h, i_max * sizeof(float), cudaMemcpyHostToDevice));
-
-    // execute kernel
+    /* Measure the start time  */
     cudaEventRecord(start, 0);
-    /* vectorAddKernel<<<n/threadBlockSize, threadBlockSize>>>(deviceA, deviceB, deviceResult); */
-    cudaEventRecord(stop, 0);
+
+    /* launch one kernel to compute, per-block, a partial maximum */
+    reduce_max_kernel<<<max_blocks, block_size, sizeof(float) * block_size>>>
+        (list_d, partial_result_d, i_max);
 
     // check whether the kernel invocation was successful
     checkCudaCall(cudaGetLastError());
 
+    /* launch a single block to compute the maximum of the partial maximums */
+    reduce_max_kernel<<<1, max_blocks, max_blocks * sizeof(float)>>>
+        (partial_result_d, partial_result_d, i_max);
+
+    /* check whether the kernel invocation was successful */
+    checkCudaCall(cudaGetLastError());
+
+    /* Stop the timing */
+    cudaEventRecord(stop, 0);
+
     // copy result back
-    checkCudaCall(cudaMemcpy(list_h, list_d, sizeof(float), cudaMemcpyDeviceToHost));
+    checkCudaCall(cudaMemcpy(&result, partial_result_d, sizeof(float), cudaMemcpyDeviceToHost));
 
     checkCudaCall(cudaFree(list_d));
+    checkCudaCall(cudaFree(partial_result_d));
 
     // print the time the kernel invocation took, without the copies!
     float elapsedTime;
     cudaEventElapsedTime(&elapsedTime, start, stop);
-
     cout << "kernel invocation took " << elapsedTime << " milliseconds" << endl;
-    return *list_h;
 }
 
 
 int main(int argc, char* argv[])
 {
-    int i_max;
+    int i_max = 0;
+    int result = 0;
 
     if (argc < 3) {
         printf("Usage: %s i_max\n", argv[0]);
@@ -106,7 +149,9 @@ int main(int argc, char* argv[])
         printf("List[%d]: %f\n", i, list[i]);
     }
 
-    cout << "max: " << max_array(list, i_max) << endl;
+    reduce_max_cuda(i_max, list, result);
+    cout << "max seq:" << max_array(list, i_max) << endl;
+    cout << "max cuda:" << result << endl;
 
     return EXIT_SUCCESS;
 }
